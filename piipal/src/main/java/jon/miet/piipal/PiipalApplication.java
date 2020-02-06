4318348c0919d6a9da51c39b1e6679d8a2dd339b
joni miettinen <joni9909@hotmail.fi>
2020-02-06 15:23:48 +0200

    ekat lisäykset

---
+++ piipal/src/main/java/jon/miet/piipal/PiipalApplication.java
@@ -0,0 +1,13 @@
+package jon.miet.piipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiipalApplication.class, args);
+	}
+
+}
---
+++ piipal/src/test/java/jon/miet/piipal/PiipalApplicationTests.java
@@ -0,0 +1,13 @@
+package jon.miet.piipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
